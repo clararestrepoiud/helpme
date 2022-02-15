614e5c2c821c5a73456fba90f16d93db96631642
Andrea <andrea.chaparro@est.iudigital.edu.co>
2022-02-14 22:24:46 -0500

    Cambios finales

--- src/main/java/co/edu/iudigital/app/controller/DelitoController.java
+++ src/main/java/co/edu/iudigital/app/controller/DelitoController.java
@@ -29,62 +29,49 @@ import io.swagger.annotations.Tag;
 @RestController
 @RequestMapping("/delitos")
 
-@Api(value="/delitos", tags= {"Delitos"})
-@SwaggerDefinition(tags = {
-		@Tag(name = "Delitos", description = "Gestion de API delitos")
-})
-/*TODO: COLOCAR ENDPOINTS CON RESPONSEENTITY<>*/
+@Api(value = "/delitos", tags = { "Delitos" })
+@SwaggerDefinition(tags = { @Tag(name = "Delitos", description = "Gestion de API delitos") })
+/* TODO: COLOCAR ENDPOINTS CON RESPONSEENTITY<> */
 public class DelitoController {
 
 	private static final Logger log = LoggerFactory.getLogger(DelitoController.class);
-	
+
 	@Autowired
 	private IDelitoService delitoService;
-	
-	/*esto es otra opcion
-	 * @Autowired
-	public DelitoController( IDelitoService delitoService) {
-		this.delitoService = delitoService
-	}*/
-	
-	@ApiOperation(value = "Obtiene todos los delitos",
-			response = DelitoDto.class,
-			responseContainer = "List",
-			produces = "application/json",
-			httpMethod = "GET")
+
+	/*
+	 * esto es otra opcion
+	 * 
+	 * @Autowired public DelitoController( IDelitoService delitoService) {
+	 * this.delitoService = delitoService }
+	 */
+
+	@ApiOperation(value = "Obtiene todos los delitos", response = DelitoDto.class, responseContainer = "List", produces = "application/json", httpMethod = "GET")
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
-	public List<DelitoDto> index(){
+	public List<DelitoDto> index() {
 		log.info("Inicio index controller");
 		return delitoService.findAll();
 	}
-	
-	@ApiOperation(value = "Obtiene un delito por su id",
-			response = Delito.class,
-			produces = "application/json",
-			httpMethod = "GET")
+
+	@ApiOperation(value = "Obtiene un delito por su id", response = Delito.class, produces = "application/json", httpMethod = "GET")
 	@ResponseStatus(code = HttpStatus.OK)
 	@GetMapping("/delito/{id}")
-	public Delito show(@PathVariable final Long id){//TODO: RETURNAR DELITODTO
+	public Delito show(@PathVariable final Long id) {// TODO: RETURNAR DELITODTO
 		return delitoService.findById(id);
 	}
-	
-	@ApiOperation(value = "Guarda un delito",
-			response = Delito.class,
-			produces = "application/json",
-			httpMethod = "POST")
+
+	@ApiOperation(value = "Guarda un delito", response = Delito.class, produces = "application/json", httpMethod = "POST")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	@PostMapping
-	public Delito create(@RequestBody @Valid Delito delito){//TODO: RETURNAR DELITODTO
+	public Delito create(@RequestBody @Valid Delito delito) {// TODO: RETURNAR DELITODTO
 		return delitoService.save(delito);
 	}
-	
-	@ApiOperation(value = "Elimina un delito por id",
-			produces = "application/json",
-			httpMethod = "POST")
+
+	@ApiOperation(value = "Elimina un delito por id", produces = "application/json", httpMethod = "POST")
 	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	@DeleteMapping("/delito/{id}")
-	public void delete(@PathVariable Long id) throws RestException{//TODO: RETURNAR DELITODTO
+	public void delete(@PathVariable Long id) throws RestException {// TODO: RETURNAR DELITODTO
 		delitoService.delete(id);
 	}
 }
---
+++ src/main/java/co/edu/iudigital/app/service/iface/ICasoService.java
@@ -0,0 +1,17 @@
+package co.edu.iudigital.app.service.iface;
+
+import java.util.List;
+
+import co.edu.iudigital.app.dto.CasoDto;
+import co.edu.iudigital.app.exception.RestException;
+import co.edu.iudigital.app.model.Caso;
+
+public interface ICasoService {
+	public List<CasoDto> findAll() throws RestException;
+
+	public Caso save(Caso caso) throws RestException;
+
+	public Boolean visible(Boolean visible, Long Id) throws RestException;
+
+	public Caso findById(Long Id) throws RestException;
+}
